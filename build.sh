bcbd9b943cb78086d5cd8d0d716e73325e3b65d1
Vilson Vieira <vilson@void.cc>
2016-12-23 00:54:41 -0200

    Add function to upload to S3

--- build.sh
+++ build.sh
@@ -13,6 +13,28 @@ export OUT_PATH=$OUT_DIR/app/vendor/vips
 export PKG_CONFIG_PATH=$OUT_PATH/lib/pkgconfig:$PKG_CONFIG_PATH
 export PATH=$OUT_PATH/bin:$PATH
 
+# Based on https://gist.github.com/chrismdp/6c6b6c825b07f680e710
+function putS3 {
+  file=$1
+  aws_path=$2
+  aws_id=$3
+  aws_token=$4
+  aws_bucket=$5
+  date=$(date +"%a, %d %b %Y %T %z")
+  acl="x-amz-acl:public-read"
+  content_type='application/x-compressed-tar'
+  string="PUT\n\n$content_type\n$date\n$acl\n/$aws_bucket$aws_path$file"
+  signature=$(echo -en "${string}" | openssl sha1 -hmac "${aws_token}" -binary | base64)
+  curl -X PUT -T "$file" \
+    -H "Host: $aws_bucket.s3.amazonaws.com" \
+    -H "Date: $date" \
+    -H "Content-Type: $content_type" \
+    -H "$acl" \
+    -H "Authorization: AWS ${aws_id}:$signature" \
+    "https://$aws_bucket.s3.amazonaws.com$aws_path$file"
+  echo "Uploaded to https://$aws_bucket.s3.amazonaws.com$aws_path$file"
+}
+
 # These should be set from the outside. A git version and heroku/travis respectively
 if [ -z "$VERSION" ]
 then
@@ -310,13 +332,12 @@ rm -rf $OUT_PATH/share/{doc,gtk-doc}
 tar -cvzf libvips-${VERSION}-${TARGET}.tgz *
 
 ###############
-#     FTP     #
+#     S3      #
 ###############
-
-if [ -z "$FTP_PASSWORD" ];
+if [ -z "$AMAZON_API_TOKEN" ];
 then
-    echo "FTP_PASSWORD not provided, skipping upload";
+    echo "Amazon API Token not provided, skipping upload";
 else
-    curl --ftp-create-dirs -T libvips-${VERSION}-${TARGET}.tgz -u ${FTP_USER}:${FTP_PASSWORD} ${FTP_SERVER}/
+    putS3 "libvips-${VERSION}-${TARGET}.tgz" "/bundles/" $AMAZON_API_ID $AMAZON_API_TOKEN $AMAZON_API_BUCKET
 fi
 
