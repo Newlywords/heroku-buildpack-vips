54d6af9e352480c83be408c1843061ff8bb18f53
Lucas Mendelowski <lucas@mendelowski.com>
2024-10-08 08:26:40 +0200

    Rebuild binaries, remove old stack, allow to run build on MacOS

--- build.sh
+++ build.sh
@@ -10,19 +10,26 @@ rm -rf ./build/*.tar.gz
 mkdir -p ./build/configurations
 rm -rf ./build/configurations/*.log
 
-STACK_VERSIONS=(20 22)
+STACK_VERSIONS=(22)
 
 for stack_version in "${STACK_VERSIONS[@]}"; do
   image_name=libvips-heroku-$stack_version:$VIPS_VERSION
 
+  echo "Building ${image_name}"
+
   docker build \
-    --build-arg VIPS_VERSION=${VIPS_VERSION} \
-    --build-arg STACK_VERSION=${stack_version} \
-    -t $image_name \
+    --build-arg "VIPS_VERSION=${VIPS_VERSION}" \
+    --build-arg "STACK_VERSION=${stack_version}" \
+    --platform linux/amd64 \
+    -t "${image_name}" \
     -f "container/Dockerfile.heroku-$stack_version" \
     container
 
   mkdir -p build
 
-  docker run --rm -t -v $PWD/build:/build $image_name sh -c 'cp -f /usr/local/build/*.tar.gz /build && cp -f /usr/local/build/*.config.log /build/configurations'
+  docker run \
+    --rm -t \
+    --platform linux/amd64 \
+    -v "${PWD}/build:/build" "${image_name}" \
+    sh -c 'cp -f /usr/local/build/*.tar.gz /build && cp -f /usr/local/build/*.config.log /build/configurations'
 done
